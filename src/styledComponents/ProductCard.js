2a4158f384637a0d12ae6510d91a70bd79b44a46
noemi <gynoemi33@gmail.com>
2020-09-02 13:22:43 +0200

    Add default image

--- src/styledComponents/ProductCard.js
+++ src/styledComponents/ProductCard.js
@@ -4,13 +4,24 @@ const ProductCard = (props) => {
   let { productName, imageUrl, price, description } = props.product;
   let imgClassNames = "img.fluid m-0";
 
+  const addDefaultSrc = (ev) => {
+    ev.target.src =
+      "https://www.hopkinsmedicine.org/-/media/feature/noimageavailable.ashx";
+  };
+
   return (
     <div className="card">
       <div className="card-body">
         <p className="card-text">{productName}</p>
       </div>
       <div className="image-container">
-        <img className={imgClassNames} width="100%" src={imageUrl} alt=""></img>
+        <img
+          className={imgClassNames}
+          onError={addDefaultSrc}
+          width="100%"
+          src={imageUrl}
+          alt=""
+        ></img>
       </div>
       <div className="card-body">
         <p className="card-text">
--- src/styledComponents/ProductList.js
+++ src/styledComponents/ProductList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import loading from "./loading.gif";
+import loading from "../images/loading.gif";
 import ProductCard from "./ProductCard";
 
 const ProductList = ({ products, productsAreLoading }) => {
