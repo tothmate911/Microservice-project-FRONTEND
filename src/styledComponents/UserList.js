752c83b0b8dc869eb35180ab6529ca3a3f0d757b
noemi <gynoemi33@gmail.com>
2020-09-03 14:52:40 +0200

    Fix Merge

--- src/styledComponents/UserList.js
+++ src/styledComponents/UserList.js
@@ -1,24 +1,22 @@
 import React from "react";
-import loading from "./loading.gif";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 
-const UserList = ({users, usersAreLoading}) => {
+const UserList = ({ users, usersAreLoading }) => {
+  let content = (
+    <div className="d-flex justify-content-center">
+      <Spinner animation="grow" variant="light" />
+    </div>
+  );
 
-    let content = (
-        <div className="d-flex justify-content-center">
-            <img src={loading} alt=""/>
-        </div>
-    );
-
-    if (!usersAreLoading && users[0]) {
-        content = (
-            users[0].map((user) => (
-                <li key={user.id}>
-                    <Link to={`/UserProduct${user.id}`}>{user.username}</Link>
-                </li>
-            )));
-    }
-    return content;
-}
+  if (!usersAreLoading && users[0]) {
+    content = users[0].map((user) => (
+      <li key={user.id}>
+        <Link to={`/UserProduct${user.id}`}>{user.username}</Link>
+      </li>
+    ));
+  }
+  return content;
+};
 
 export default UserList;
